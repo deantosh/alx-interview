78c5f6ca05f4e580a62ae3cb204d5bc8b1b032b4
deantosh <deantoshdaiddoh@gmail.com>
2024-10-06 19:11:31 +0000

    solution: Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,14 @@
-# Create a function  that returns a list of lists of integers representing
-# the Pascal’s triangle of n:
-# Returns an empty list if n <= 0
-# You can assume n will be always an integer
+#!/usr/bin/python3
+"""
+Create a function  that returns a list of lists of integers representing
+the Pascal’s triangle of n:
+ - Returns an empty list if n <= 0
+ - You can assume n will be always an integer
+"""
 
 
 def pascal_triangle(n):
+    """ Pascal triangle """
     if n <= 0:
         return []
 
